7415cf5adfa7140bf48ae2ba45c55d1d890b3059
Avery <averyyip@ymail.com>
2017-11-03 16:10:49 -0700

    Updated sleep to prevent ban

--- translate_html.py
+++ translate_html.py
@@ -2,45 +2,61 @@ from googletrans import Translator
 import glob2
 from multiprocessing import Pool
 from bs4 import BeautifulSoup
+from time import gmtime, strftime, sleep
+ 
 
-def run(html_path):
-	html_paths = glob2.glob(html_path + "*.html")
+#Better documentation + check output folder if file already exists, if so skip
+
+html_folder_path = "copenhagen_htmls\\"
+output_folder_path = "copenhagen_translated_htmls\\"
+
+def run():
+	#html_paths = glob2.glob(html_folder_path + "*.html")
+	html_paths = ["copenhagen_htmls\\13.html"]
 	p = Pool(5)
 	p.map(translator, html_paths)
 
 
 def translator(html_path):
-	translator = Translator()
-	all_text = []
-	translated = []
-	output_path = output_folder_path + html_path[len(html_folder_path):]
-	file_data = open(html_path, encoding="utf-8").read() #try latin1
+	try:
+		#Sleep to prevent google ip ban
+		sleep(1)
 
-	soup = BeautifulSoup(file_data)
-	all_ns = soup.body.find_all(text=True)
+		#Initialize class and all_text will contain our translated text
+		translator = Translator()
+		all_text = []
 
-	for element in all_ns:
-		text = element.string
-		text = text.replace(u"\u2026", "") #for handling weird windows encoding
-		if text:
-			all_text.append(text)
+		#Read HTML file
+		output_path = output_folder_path + html_path[len(html_folder_path):]
+		file_data = open(html_path, encoding="utf-8").read() #try latin1
+		soup = BeautifulSoup(file_data)
 
-	translated = translator.translate(all_text, dest=trans_lang)
+		#Translate text
+		all_ns = soup.body.findAll(text=True)
+		for element in all_ns:
+			text = element.string
+			text = text.replace(u"\u2026", "")
+			text = text.replace(u"\xa0", "") #test for nbsp in latin
+			if text:
+				all_text.append(text)
 
-	for i in range(len(all_ns)):
-		element = all_ns[i]
-		element.replace_with(translated[i].text)
+		all_text = translator.translate(all_text, dest="en")
 
-	with open(output_path, "wb") as file:
-		file.write(soup.prettify("utf-8"))
+		#Replace text in html file
+		for i in range(len(all_ns)):
+			element = all_ns[i]			
+			element.replace_with(all_text[i].text)
 
-def translate_html(html_folder, output_folder, lang="en"):
-	global trans_lang
-	trans_lang = lang
-	run(html_folder)
+		#Save translated html file
+		with open(output_path, "wb+") as file:
+			file.write(soup.prettify("utf-8"))
+
+	#Catch any errors and append it to a error log file
+	except Exception as e:
+		with open("translate_log.txt", "a+") as log:
+			curr_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+			log.write(curr_time + "," + html_path + "," + str(e) + "\n")
 
 if __name__ == "__main__":
-	output_path = "copenhagen_translated_htmls\\"
-	input_path = "copenhagen_htmls\\"
-	translate_html(input_path)
+	run()
 	print("Complete")
